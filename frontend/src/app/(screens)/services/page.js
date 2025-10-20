119506cba788395db80c16d04bc2bcec9a000f1c
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2025-10-20 13:44:21 -0400

    Update page.js

--- frontend/src/app/(screens)/services/page.js
+++ frontend/src/app/(screens)/services/page.js
@@ -1,5 +1,9 @@
 const ServicesPage = () => {
-  return <div>Enter</div>;
+  return (
+    <main className="p-4 bg-background pt-24">
+      <h1>Services</h1>
+    </main>
+  );
 };
 
 export default ServicesPage;
