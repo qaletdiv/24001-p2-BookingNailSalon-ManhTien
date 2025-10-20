f4adbb0b8d216174fa7369ba98e17cca0c4057d6
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2025-10-20 16:02:00 -0400

    Update page.js

--- frontend/src/app/(screens)/services/page.js
+++ frontend/src/app/(screens)/services/page.js
@@ -1,7 +1,17 @@
+import { Tangerine } from "next/font/google";
+const tangerine = Tangerine({
+  weight: ["400", "700"],
+  variable: "--font-tangerine",
+  subsets: ["latin"],
+});
 const ServicesPage = () => {
   return (
     <main className="p-4 bg-background pt-24">
-      <h1>Services</h1>
+      <h1
+        className={`${tangerine.className} text-center py-12 text-6xl font-bold text-neutral-900`}
+      >
+        <em>Services</em>
+      </h1>
     </main>
   );
 };
