838b95ae743f30689694369bed9a1a324979830a
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2025-10-21 09:50:14 -0400

    page.js Homepage : add fetch services, file loading.js: add sample

---
+++ frontend/src/app/loading.js
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <h1 className="text-9xl">Loading ......</h1>
+    </div>
+  );
+}
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,8 +1,17 @@
 import Hero from "@/components/layout/Hero/Hero";
-import Counter from "../components/Counter";
-import StoreProvider from "./StoreProvider";
 
-export default function Home() {
+import StoreProvider from "./StoreProvider";
+async function fetchServicesData() {
+  const res = await fetch(`${process.env.API_SERVER}/services`);
+  if (!res.ok)
+    throw new Error(`Can't fetch data from the server! Something went wrong.`);
+  const services = await res.json();
+  return services;
+}
+
+export default async function Home() {
+  const services = await fetchServicesData();
+  console.log(services);
   return (
     <>
       <StoreProvider>
