5caacec48fc911fbcc8e45008b20bf6395612d92
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2025-10-21 21:50:34 -0400

    Update ServicesHomePage.js
    add slider

--- frontend/src/components/layout/homepage/services/ServicesHomePage.js
+++ frontend/src/components/layout/homepage/services/ServicesHomePage.js
@@ -1,47 +1,76 @@
+"use client";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import Link from "next/link";
-
+import { useEffect, useRef, useState } from "react";
+const categories = ["pedicure", "manicure", "enhancement", "addon", "waxing"];
 const ServicesHomePage = () => {
+  const [index, setIndex] = useState(0);
+  const timeoutRef = useRef();
+  useEffect(() => {
+    timeoutRef.current = setTimeout(() => {
+      setIndex((i) => (i + 1) % categories.length);
+    }, 3000);
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, [index]);
+  const handleLeftChevron = () => {
+    setIndex((i) => (i - 1 + categories.length) % categories.length);
+  };
+  const handleRightChevron = () => {
+    setIndex((i) => (i + 1) % categories.length);
+  };
   return (
-    <section>
+    <section className="px-4 md:px-16">
       <div className="text-center py-8">
         <h1 className="font-tangerine text-7xl font-extrabold">Our Services</h1>
         <p className="text-sm">
           Your nails are a blank canvas waiting for creattivity.
         </p>
-        <Link href={"/services"} className="my-6">
+        <Link href={"/services"} className="">
           READ MORE
         </Link>
       </div>
-      <div>
-        <div className="h-72">
-          <div>image</div>
-          <div>
-            <Link href={"/services/#pedicure"}>Pedicure</Link>
-          </div>
-        </div>
-        <div>
-          <div>image</div>
-          <div>
-            <Link href={"/services/#medicure"}>Medicure</Link>
-          </div>
-        </div>
-        <div>
-          <div>image</div>
-          <div>
-            <Link href={"/services/#enhancement"}>Enhancement</Link>
-          </div>
-        </div>
-        <div>
-          <div>image</div>
-          <div>
-            <Link href={"/services/#addon"}>Add On</Link>
-          </div>
-        </div>
-        <div>
-          <div>image</div>
-          <div>
-            <Link href={"services/#waxing"}>Waxing</Link>
-          </div>
+      {/* Slider Container */}
+      <div className="h-96 relative w-full overflow-hidden ">
+        {/* Navigation Buttons */}
+        <button
+          className="z-45 absolute left-[1%] top-[40%]"
+          onClick={handleLeftChevron}
+        >
+          <ChevronLeft className="size-10" />
+        </button>
+        <button
+          onClick={handleRightChevron}
+          className="z-45 absolute right-[1%] top-[40%] "
+        >
+          <ChevronRight className="size-10" />
+        </button>
+        {/* Slides Container */}
+        <div
+          className="flex h-full w-full transition-transform duration-500 ease-out "
+          style={{ transform: `translateX(-${index * 100}%)` }}
+        >
+          {categories.map((category, idx) => {
+            return (
+              <div
+                key={idx}
+                className="h-full min-w-full relative flex flex-col justify-between "
+              >
+                <div className="bg-foreground h-full ">image</div>
+                <div className="bg-black text-center py-4">
+                  <Link
+                    href={`/services/#${category}}`}
+                    className="text-white font-bold text-2xl"
+                  >
+                    {`${category.charAt(0).toUpperCase()}${category.slice(1)}`}
+                  </Link>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
