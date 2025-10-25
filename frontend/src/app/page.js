8802fd790752cfde8d2c45ee2d29a637bb881d86
TienPhamDev <manhtienbn18891@gmail.com>
2025-10-25 00:55:25 -0400

    create Booking page, move fetchServicesData funtions to fuctions

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -6,13 +6,7 @@ import AboutHomePage from "@/components/layout/homepage/about/AboutHomePage";
 import PriceListHomePage from "@/components/layout/homepage/pricelist/PriceListHomePage";
 import GalleryHomePage from "@/components/layout/homepage/gallery/GalleryHomePage";
 import Image from "next/image";
-async function fetchServicesData() {
-  const res = await fetch(`${process.env.API_SERVER}/services`);
-  if (!res.ok)
-    throw new Error(`Can't fetch data from the server! Something went wrong.`);
-  const services = await res.json();
-  return services;
-}
+import { fetchServicesData } from "@/functions/fetchServicesData";
 
 export default async function Home() {
   const services = await fetchServicesData();
