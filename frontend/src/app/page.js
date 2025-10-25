d4bac7111c8f5e6d3765581cc60d7e8eda2ab46a
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2025-10-25 15:06:24 -0400

    import DataHydrator, temp delete loading

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,20 +1,19 @@
 import Hero from "@/components/layout/Hero/Hero";
 
-import StoreProvider from "./StoreProvider";
 import ServicesHomePage from "@/components/layout/homepage/services/ServicesHomePage";
 import AboutHomePage from "@/components/layout/homepage/about/AboutHomePage";
 import PriceListHomePage from "@/components/layout/homepage/pricelist/PriceListHomePage";
 import GalleryHomePage from "@/components/layout/homepage/gallery/GalleryHomePage";
 import { fetchServicesData } from "@/functions/fetchServicesData";
 import { fetchStaffData } from "@/functions/fetchStaffData";
-
+import DataHydrator from "@/components/datahydrator/DataHyrator";
 export default async function Home() {
   const services = await fetchServicesData();
   const staff = await fetchStaffData();
   return (
     <>
-        {/* Hero*/}
         <DataHydrator services={services} staff={staff} />
+        {/* Hero*/}
         <Hero />
         <AboutHomePage />
         <ServicesHomePage />
