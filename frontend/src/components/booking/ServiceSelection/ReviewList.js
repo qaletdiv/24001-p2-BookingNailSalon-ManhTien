b6718a7d138548a350417a1a54533d155468784f
TienPhamDev <manhtienbn18891@gmail.com>
2025-11-04 01:07:02 -0500

    update
    reviewlist change text size

--- frontend/src/components/booking/ServiceSelection/ReviewList.js
+++ frontend/src/components/booking/ServiceSelection/ReviewList.js
@@ -23,36 +23,40 @@ const ReviewList = () => {
           Your can add more services by clicking the Add Service button.
         </p>
       </div>
-      {currentSelected.map((selected, idx) => (
-        <div
-          key={idx}
-          className="lg:w-1/2 md:w-[500px] mx-auto shadow-md flex  justify-between items-center bg-white border border-gray-300 text-neutral-900 p-4 rounded-lg"
-        >
-          <div className="flex flex-col justify-between items-start">
-            <span className="font-bold text-2xl">{selected.ServiceName}</span>
-            <span className="text-lg text-gray-700">
-              Technician: {selected.StaffName}
-            </span>
-            <span className="text-lg text-gray-700">
-              Duration: {selected.duration} minutes
-            </span>
-            <span className="text-lg text-gray-700">
-              Price: ${selected.price}
-            </span>
+      <div className="flex flex-col gap-4 w-full">
+        {currentSelected.map((selected, idx) => (
+          <div
+            key={idx}
+            className="lg:w-3/4 md:w-[500px] w-full mx-auto shadow-md flex  justify-between items-center bg-white border border-gray-300 text-neutral-900 p-4 rounded-lg"
+          >
+            <div className="flex flex-col justify-between items-start">
+              <span className="font-bold text-2xl ">
+                {selected.ServiceName}
+              </span>
+              <span className="text-lg text-gray-700">
+                Technician: {selected.StaffName}
+              </span>
+              <span className="text-lg text-gray-700">
+                Duration: {selected.duration} minutes
+              </span>
+              <span className="text-lg text-gray-700">
+                Price: ${selected.price}
+              </span>
+            </div>
+            <div className="flex flex-col gap-2">
+              <button className="bg-foreground text-neutral-900 px-4 py-2 rounded-full text-sm md:text-base">
+                Change Service
+              </button>
+              <button className="bg-foreground text-neutral-900 px-4 py-2 rounded-full text-sm md:text-base">
+                Change Technician
+              </button>
+              <button className="bg-foreground text-neutral-900 px-4 py-2 rounded-full text-sm md:text-base">
+                Remove
+              </button>
+            </div>
           </div>
-          <div className="flex flex-col gap-2">
-            <button className="bg-foreground text-neutral-900 px-4 py-2 rounded-full">
-              Change Service
-            </button>
-            <button className="bg-foreground text-neutral-900 px-4 py-2 rounded-full">
-              Change Technician
-            </button>
-            <button className="bg-foreground text-neutral-900 px-4 py-2 rounded-full">
-              Remove
-            </button>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </>
   );
 };
