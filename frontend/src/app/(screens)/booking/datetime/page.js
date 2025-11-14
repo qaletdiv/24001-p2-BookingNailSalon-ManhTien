a609c0dfb3bf66997809f5545bd69220e1b8ba75
TienPhamDev <manhtienbn18891@gmail.com>
2025-11-13 22:18:25 -0500

    update
    create fetchAppointmentData
    create ArrTimeSlot in DateTime component
    change: render time slot from ArrTimeSlot
    update: sample data appointmen in server.
    update : call fetchAppointmentData in DateTimePage

--- frontend/src/app/(screens)/booking/datetime/page.js
+++ frontend/src/app/(screens)/booking/datetime/page.js
@@ -1,13 +1,13 @@
 import DateTime from "@/components/booking/datetime/DateTime";
-const DatetimePage = () => {
+import fetchAppointmentData from "@/functions/fetchAppointmentData";
+export default async function DatetimePage() {
+  const appointmentData = await fetchAppointmentData();
   return (
     <>
       <h1 className="text-xl font-bold text-center  w-1/2 mx-auto  text-neutral-900 px-4 py-2 border-b border-gray-400">
         Date and Time
       </h1>
-      <DateTime />
+      <DateTime appointmentData={appointmentData} />
     </>
   );
-};
-
-export default DatetimePage;
+}
--- frontend/src/components/booking/datetime/DateTime.js
+++ frontend/src/components/booking/datetime/DateTime.js
@@ -4,21 +4,86 @@ import { useState, useEffect } from "react";
 import { setTime, setSelectedDate } from "@/redux/slices/bookingSlice";
 import { useAppDispatch } from "@/redux/hooks";
 import { useSelector } from "react-redux";
-const DateTime = () => {
+const DateTime = ({ appointmentData }) => {
+  const ArrTimeSlot = [
+    "9:00 AM",
+    "9:15 AM",
+    "9:30 AM",
+    "9:45 AM",
+    "10:00 AM",
+    "10:15 AM",
+    "10:30 AM",
+    "10:45 AM",
+    "11:00 AM",
+    "11:15 AM",
+    "11:30 AM",
+    "11:45 AM",
+    "12:00 PM",
+    "12:15 PM",
+    "12:30 PM",
+    "12:45 PM",
+    "1:00 PM",
+    "1:15 PM",
+    "1:30 PM",
+    "1:45 PM",
+    "2:00 PM",
+    "2:15 PM",
+    "2:30 PM",
+    "2:45 PM",
+    "3:00 PM",
+    "3:15 PM",
+    "3:30 PM",
+    "3:45 PM",
+    "4:00 PM",
+    "4:15 PM",
+    "4:30 PM",
+    "4:45 PM",
+    "5:00 PM",
+    "5:15 PM",
+    "5:30 PM",
+    "5:45 PM",
+    "6:00 PM",
+    "6:15 PM",
+    "6:30 PM",
+  ];
   const dispatch = useAppDispatch();
+  const [date, setDate] = useState(new Date().toDateString());
+  const [timeButtonSelected, setTimeButtonSelected] = useState(null);
   const staffData = useSelector((state) => state.staff.staff);
   const { currentSelected } = useSelector(
     (state) => state.booking.currentBooking
   );
-  console.log(staffData);
-  console.log(currentSelected);
-  const [date, setDate] = useState(
-    new Date().toLocaleDateString("en-US", {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    })
+
+  //--------------------------------- Filter data ---------------------------------
+  // Get current selected staff id
+  const currentSelectedStaffId = currentSelected.map(
+    (selected) => selected.StaffId
   );
+  // Filter staff data based on current selected staff id
+  const filteredStaffData = staffData.filter((staff) =>
+    currentSelectedStaffId.includes(staff.id)
+  );
+  // Filter appointment data based on current selected staff
+  const filteredAppointmentData = appointmentData.filter((appointment) =>
+    currentSelectedStaffId.includes(appointment.staff.id)
+  );
+  console.log(filteredAppointmentData);
+  // Filter appointment data based on date
+  const filteredAppointmentByCurrentDate = filteredAppointmentData.filter(
+    (appointment) => {
+      const curDate = new Date(date);
+      const formated = `${curDate.getFullYear()}-${
+        curDate.getMonth() + 1
+      }-${curDate.getDate()}`;
+      return appointment.date === formated;
+    }
+  );
+  console.log("ngay thoi diem dang duoc chon:", date);
+  console.log("luc array ngay:", filteredAppointmentByCurrentDate);
+
+  //----------------------------schedule--------------------------
+
+  // Check if the date is disabled
   const isDateDisabled = (date) => {
     const today = new Date();
     today.setHours(0, 0, 0, 0);
@@ -26,7 +91,7 @@ const DateTime = () => {
     checkDate.setHours(0, 0, 0, 0);
     return checkDate < today;
   };
-  const [timeButtonSelected, setTimeButtonSelected] = useState(null);
+
   const handleTimeSelection = (time) => {
     setTimeButtonSelected(time);
     dispatch(setTime(time));
@@ -47,274 +112,84 @@ const DateTime = () => {
     setDate(date);
   };
   return (
-    <div className="flex md:flex-row w-full flex-col gap-4 justify-start items-start">
-      <Calendar
-        className="w-full md:w-1/2 "
-        mode="single"
-        selected={date}
-        onSelect={(date) => handleDateSelection(date)}
-        initialFocus
-        disabled={isDateDisabled}
-      />
-      <div id="time" className="flex flex-col gap-4 md:w-1/2 w-full mt-4">
-        <div id="morning-slots" className="flex flex-col gap-4">
-          <h3 className="text-xl md:text-md md:text-left text-center">
-            Morning
-          </h3>
-          <div className="grid grid-cols-3 lg:grid-cols-4 gap-1">
-            <button
-              className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("9:00 AM")}
-            >
-              9:00 AM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("9:15 AM")}
-            >
-              9:15 AM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("9:30 AM")}
-            >
-              9:30 AM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("9:45 AM")}
-            >
-              9:45 AM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("10:00 AM")}
-            >
-              10:00 AM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("10:15 AM")}
-            >
-              10:15 AM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("10:30 AM")}
-            >
-              10:30 AM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("10:45 AM")}
-            >
-              10:45 AM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("11:00 AM")}
-            >
-              11:00 AM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("11:15 AM")}
-            >
-              11:15 AM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("11:30 AM")}
-            >
-              11:30 AM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("11:45 AM")}
-            >
-              11:45 AM
-            </button>
+    <>
+      <div>
+        Current Selected Staff:{" "}
+        {filteredStaffData.map((staff) => staff.name).join(", ")}
+      </div>
+      <div className="flex md:flex-row w-full flex-col gap-4 justify-start items-start">
+        <Calendar
+          className="w-full md:w-1/2 "
+          mode="single"
+          selected={date}
+          onSelect={(date) => handleDateSelection(date)}
+          initialFocus
+          disabled={isDateDisabled}
+        />
+        <div id="time" className="flex flex-col gap-4 md:w-1/2 w-full mt-4">
+          <div id="morning-slots" className="flex flex-col gap-4">
+            <h3 className="text-xl md:text-md md:text-left text-center">
+              Morning
+            </h3>
+            <div className="grid grid-cols-3 lg:grid-cols-4 gap-1">
+              {ArrTimeSlot.map((time, idx) => {
+                if (/^(9|10|11):/.test(time)) {
+                  return (
+                    <button
+                      key={idx}
+                      className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
+                      onClick={() => handleTimeSelection(time)}
+                    >
+                      {time}
+                    </button>
+                  );
+                }
+              })}
+            </div>
           </div>
-        </div>
-        <div id="afternoon-slots" className="flex flex-col gap-4">
-          <h3 className="text-xl md:text-md  md:text-left text-center">
-            Afternoon{" "}
-          </h3>
-          <div className="grid grid-cols-3 lg:grid-cols-4 gap-1">
-            <button
-              className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("12:00 PM")}
-            >
-              12:00 PM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("12:15 PM")}
-            >
-              12:15 PM
-            </button>
-
-            <button
-              className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("12:30 PM")}
-            >
-              12:30 PM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("12:45 PM")}
-            >
-              12:45 PM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("1:00 PM")}
-            >
-              1:00 PM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("1:15 PM")}
-            >
-              1:15 PM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("1:30 PM")}
-            >
-              1:30 PM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("1:45 PM")}
-            >
-              1:45 PM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("2:00 PM")}
-            >
-              2:00 PM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("2:15 PM")}
-            >
-              2:15 PM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("2:30 PM")}
-            >
-              2:30 PM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("2:45 PM")}
-            >
-              2:45 PM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("3:00 PM")}
-            >
-              3:00 PM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("3:15 PM")}
-            >
-              3:15 PM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("3:30 PM")}
-            >
-              3:30 PM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("3:45 PM")}
-            >
-              3:45 PM
-            </button>
+          <div id="afternoon-slots" className="flex flex-col gap-4">
+            <h3 className="text-xl md:text-md  md:text-left text-center">
+              Afternoon{" "}
+            </h3>
+            <div className="grid grid-cols-3 lg:grid-cols-4 gap-1">
+              {ArrTimeSlot.map((time, idx) => {
+                if (/^(12|1|2|3):/.test(time)) {
+                  return (
+                    <button
+                      key={idx}
+                      className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
+                      onClick={() => handleTimeSelection(time)}
+                    >
+                      {time}
+                    </button>
+                  );
+                }
+              })}
+            </div>
           </div>
-        </div>
-        <div id="evening-slots" className="flex flex-col gap-4">
-          <h3 className="text-xl md:text-md  md:text-left text-center">
-            Evening{" "}
-          </h3>
-          <div className="grid grid-cols-3 lg:grid-cols-4 gap-2">
-            <button
-              className=" py-2 md:w-[78px] w-full  text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("4:00 PM")}
-            >
-              4:00 PM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full  text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("4:15 PM")}
-            >
-              4:15 PM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full  text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("4:30 PM")}
-            >
-              4:30 PM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full  text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("4:45 PM")}
-            >
-              4:45 PM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full  text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("5:00 PM")}
-            >
-              5:00 PM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full  text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("5:15 PM")}
-            >
-              5:15 PM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("5:30 PM")}
-            >
-              5:30 PM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full  text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("5:45 PM")}
-            >
-              5:45 PM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full  text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("6:00 PM")}
-            >
-              6:00 PM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full  text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("6:15 PM")}
-            >
-              6:15 PM
-            </button>
-            <button
-              className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
-              onClick={() => handleTimeSelection("6:30 PM")}
-            >
-              6:30 PM
-            </button>
+          <div id="evening-slots" className="flex flex-col gap-4">
+            <h3 className="text-xl md:text-md  md:text-left text-center">
+              Evening{" "}
+            </h3>
+            <div className="grid grid-cols-3 lg:grid-cols-4 gap-1">
+              {ArrTimeSlot.map((time, idx) => {
+                if (/^(4|5|6):/.test(time)) {
+                  return (
+                    <button
+                      key={idx}
+                      className=" py-2 md:w-[78px] w-full text-center rounded-md bg-foreground text-neutral-900"
+                      onClick={() => handleTimeSelection(time)}
+                    >
+                      {time}
+                    </button>
+                  );
+                }
+              })}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
