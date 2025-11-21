e7869ec681447b8fc22b1e3a26ed675a3af0574e
TienPhamDev <manhtienbn18891@gmail.com>
2025-11-20 22:37:25 -0500

    Create page.js

---
+++ frontend/src/app/(screens)/booking/customer/page.js
@@ -0,0 +1,11 @@
+const CustomerPage = () => {
+  return (
+    <>
+      <h1 className="text-xl font-bold text-center  w-1/2 mx-auto  text-neutral-900 px-4 py-2 border-b border-gray-400">
+        Customer
+      </h1>
+    </>
+  );
+};
+
+export default CustomerPage;
